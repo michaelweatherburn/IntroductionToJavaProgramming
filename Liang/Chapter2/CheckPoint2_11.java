e228e4f32c1816c1497e46208cd33c235741944a
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-04-07 16:45:13 +0530

    Completed some of the implicit type casting rule tables

--- Liang/Chapter2/CheckPoint2_11.java
+++ Liang/Chapter2/CheckPoint2_11.java
@@ -1,9 +1,9 @@
 public class CheckPoint2_11{
   public static void main(String[] args)
   {
-    System.out.println(   56 % 6);
+    System.out.println(56 % 6);
     System.out.println( 78 % -4);
-    System.out.println(-34%-5);
-    System.out.println(-19%5);
+    System.out.println(-34 % -5);
+    System.out.println(-19 % 5);
   }
 }
