40c524ba5345e289bb7a8a0efbb76cbc8e1d536d
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-04-25 13:21:59 +0530

    Continuning Chapter3 Exercise

---
+++ Liang/Chapter3/Question3_7.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Question3_7
+{
+  public static void main(String[] args)
+  {
+    Scanner scannerObject = new Scanner(System.in);
+
+    double amount;
+    int numberOfOneDollars, numberOfCents, numberOfPennies, numberOfNickels, numberOfQuarters, numberOfDimes;
+
+    System.out.print("Enter the amount. ");
+    amount = scannerObject.nextDouble();
+
+    numberOfCents = (int)(amount * 100); //filtered out the unnecessary digits
+    numberOfOneDollars = numberOfCents / 100;
+    numberOfCents %= 100;
+
+    numberOfQuarters = numberOfCents / 25;
+    numberOfCents %= 25;
+
+    numberOfDimes = numberOfCents / 10;
+    numberOfCents %= 10;
+
+    numberOfNickels = numberOfCents / 5;
+    numberOfCents %= 5;
+
+    numberOfPennies = numberOfCents;
+
+    //now the interesting part: check which of these have values greater than 0
+    System.out.println("Your amount " + amount + " consists of: ");
+
+    if(numberOfOneDollars == 1)
+      System.out.println(numberOfOneDollars + " Dollar");
+    else if(numberOfOneDollars > 1)
+      System.out.println(numberOfOneDollars + " Dollars");
+
+    if(numberOfQuarters == 1)
+      System.out.println(numberOfQuarters + " Quarter");
+    else if(numberOfOneDollars > 1)
+      System.out.println(numberOfQuarters + " Quarters");
+
+    if(numberOfDimes == 1)
+      System.out.println(numberOfDimes + " Dime");
+    else if(numberOfDimes > 1)
+      System.out.println(numberOfDimes + " Dimes");
+
+    if(numberOfNickels == 1)
+      System.out.println(numberOfNickels + " Nickel");
+    else if(numberOfNickels > 1)
+      System.out.println(numberOfNickels + " Nickels");
+
+    if(numberOfPennies == 1)
+      System.out.println(numberOfPennies + " Penny");
+    else if(numberOfPennies > 1)
+      System.out.println(numberOfPennies + " Pennies");
+
+      //again, a lot of code that could have avoided duplication, if only we knew methods. I am thinking of revisiting all these questions once methods are done
+  }
+}
---
+++ Liang/Chapter3/Question3_8.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Question3_8
+{
+  public static void main(String[] args)
+  {
+    Scanner scannerObject = new Scanner(System.in);
+
+    int firstNumber, secondNumber, thirdNumber;
+
+    System.out.print("Enter three integers consecutively: ");
+    firstNumber = scannerObject.nextInt();
+    secondNumber = scannerObject.nextInt();
+    thirdNumber = scannerObject.nextInt();
+
+
+    //what is the best way to sort three numbers (if you can't have them as an array)?
+    System.out.print("Sorted Order: ");
+    if(firstNumber > secondNumber)
+    {
+      //figure out where in the three positions does c fit into
+      if(thirdNumber < secondNumber)
+      {
+        System.out.println(thirdNumber + " " + secondNumber + " " + firstNumber);
+      }
+      else if(thirdNumber > firstNumber)
+      {
+        System.out.println(secondNumber + " " + firstNumber + " " + thirdNumber);
+      }
+      else
+      {
+        System.out.println(secondNumber + " " + thirdNumber + " " + firstNumber);
+      }
+    }
+    else  //assume that secondNumber > firstNumber
+    {
+      //figure out where c fits into
+      if(thirdNumber < firstNumber)
+      {
+        System.out.println(thirdNumber + " " + firstNumber + " " + secondNumber);
+      }
+      else if(thirdNumber > secondNumber)
+      {
+        System.out.println(firstNumber + " " + secondNumber + " " + thirdNumber);
+      }
+      else
+      {
+        System.out.println(firstNumber + " " + thirdNumber + " " + secondNumber);
+      }
+    }
+  }
+}
