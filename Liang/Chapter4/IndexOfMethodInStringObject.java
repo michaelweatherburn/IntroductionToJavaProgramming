c2011095565aed28fcef32b3ece9e39e8df8740e
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-05-26 13:08:56 +0530

    indexof() and substring() methods

---
+++ Liang/Chapter4/IndexOfMethodInStringObject.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class IndexOfMethodInStringObject
+{
+  public static void main(String[] args)
+  {
+    /* the stringObject.indexOf() method can be used in four(!) different ways.
+
+    Here's the four prototypes of the overloaded method:
+
+    indexOf(char ch)
+    This returns the first occurence of the character ch from the String. If a solution is found, indexOf returns a number between 0 and (stringObject.length() - 1). If no instance of the given string is found, the number -1 is returned.
+
+    indexOf(char ch, int fromIndex)
+    Similar to the first type, but starts search forward from the index provided by fromIndex, not from the index 0.
+
+    indexOf(String s)
+    This format searches for the occurence of a substring inside the current string.
+
+    indexOf(String s, int fromIndex)
+    This format, analogous to the second one, searches for a substring, forward from the index fromIndex.
+    */
+
+    String greeting = "hey there!";
+    System.out.println(greeting.indexOf('!'));  // 9
+    System.out.println(greeting.indexOf('!', 5));
+    System.out.println(greeting.indexOf('h'));  // 0
+    System.out.println(greeting.indexOf('h', 0)); // 0. Notice that the fromIndex is also considered when counting forward from it.
+    System.out.println(greeting.indexOf('h', 1)); //now that the second character 'e' is the symbol we're counting forward from, the first character 'h' is not processed.
+    System.out.println(greeting.indexOf("there"));  // 4. Notice that the first index of the substring is returned on the index match
+    System.out.println(greeting.indexOf("re")); // 7
+    System.out.println(greeting.indexOf("he"));   // 0
+    System.out.println(greeting.indexOf("he", 1));  //  
+  }
+}
---
+++ Liang/Chapter4/SubstringMethodInStringObject.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class SubstringMethodInStringObject
+{
+  public static void main(String[] args)
+  {
+    String testString = "Welcome";
+    /* okay, this is a tricky one to remember.
+
+    The substring method is used to extract substrings from the current string. The syntax is as follows:
+
+    stringObject.substring(a, b);
+
+    Where,
+    a = the index on which you want to begin (i.e. if you wish to begin at the 3rd character of the String, this would be 2, since the convention is zero-indexed)
+    b = the index ONE MORE THAN the index on which you wish to end. For example, the first three characters of a stringObject can be extracted like this:
+
+    stringObject.substring(0, 3);
+    Yes, this will extract from 0 to 2.
+
+    There's another syntax, which covers from the character in concern to the END of the string:
+
+    stringObject.substring(a);
+
+    This counts from the a-th character (0-indexed system) to the end of the main string. This kind of makes the two overloaded methods complements of each other, which would make it cleaner to remember.
+
+    stringObject.substring(0, a); //goes from 0 to (a-1)
+    stringObject.substring(a); //goes from a to the end of the string, this covers exactly the characters of the main string NOT covered by the first syntax.
+
+    W e l c o m e
+    0 1 2
+          3 4 5 6
+    "Welcome".substring(0,3) has output "Wel".
+    "Welcome".substring(3) has output "come".
+
+    "Welcome" = "Welcome".substring(0,3) + "Welcome".substring(3)
+
+    In general, stringObject = stringObject.substring(0, index) + stringObject.substring(index)
+    provided index is reasonably within the String i.e.
+    0 <= index <= stringObject.length()
+
+    Note that you can try to access a substring of the given string where
+    index = stringObject.length(), because it's complement has to exist.
+
+    So if stringObject.substring(0, stringObject.length()) will give you the entire
+    length of the main string, its' complement must return a null string.
+
+    That is why StringObject.substring(stringObject.length()) is a valid (empty)
+    string, even if we're asking us to give us a string that starts at stringObject.length() (we should only be normally able to select from 0 to
+    stringObject.length() - 1)
+    */
+
+    System.out.println(testString.substring(0,0));  //empty string
+    System.out.println(testString.substring(2,2));  //empty string
+    System.out.println(testString.substring(0,3));
+    System.out.println(testString.substring(2,5));  //three characters only
+    System.out.println(testString.substring(0, testString.length()));
+    System.out.println(testString.substring(testString.length()));
+    System.out.println(testString.substring(testString.length(), testString.length()));
+
+    //runtime errors follow:
+    System.out.println(testString.substring(0,testString.length() + 1));  //out of bounds
+    System.out.println(testString.substring(testString.length() + 1));  //out of bounds
+    System.out.println(testString.substring(5,4)); //a cannot be greater than b
+  }
+}
