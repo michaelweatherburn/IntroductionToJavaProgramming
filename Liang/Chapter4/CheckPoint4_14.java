9d5c558b150184f086a2d22f2848c0c7c1a9e60e
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-06-21 20:48:28 +0530

    Finished all checkpoints in Chapter 4, entered Case Studies

--- Liang/Chapter4/CheckPoint4_14.java
+++ Liang/Chapter4/CheckPoint4_14.java
@@ -2,6 +2,6 @@ public class CheckPoint4_14
 {
   public static void main(String[] args)
   {
-    System.out.println((char)((int)Math.floor(Math.random() * ('z' - 'a')) + 'a'));
+    System.out.println((char)Math.floor(Math.random() * ('z' - 'a') + 'a'));
   }
 }
---
+++ Liang/Chapter4/CheckPoint4_18.java
@@ -0,0 +1,11 @@
+public class CheckPoint4_18
+{
+  public static void main(String[] args)
+  {
+    System.out.println("1" + 1);
+    System.out.println('1' + 1);
+    System.out.println("1" + 1 + 1);
+    System.out.println("1" + (1 + 1));
+    System.out.println('1' + 1 + 1);
+  }
+}
