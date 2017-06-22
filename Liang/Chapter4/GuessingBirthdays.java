19ff5e08b203fd7355cbc6fe80bc96ab188a05bb
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-06-22 11:25:19 +0530

    Started Chapter 4: Case Studies

---
+++ Liang/Chapter4/GuessingBirthdays.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class GuessingBirthdays
+{
+  public static void main(String[] args)
+  {
+    Scanner scannerObject = new Scanner(System.in);
+
+    String set1 = "1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31";
+    String set2 = "2 3 6 7 10 11 14 15 18 19 22 23 26 27 30 31";
+    String set3 = "4 5 6 7 12 13 14 15 18 19 22 23 27 28 30 31";
+    String set4 = "8 9 10 11 12 13 14 15 24 25 26 27 28 29 30 31";
+    String set5 = "16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31";
+
+    System.out.println("There'll be five questions of the yes/no type. Please answer with a single character: Y/N.");
+
+    System.out.println("Is the number any one of these? " + set1);
+    boolean isWithinFirstSet = scannerObject.next().charAt(0) == 'Y' ? true : false;
+
+    System.out.println("Is the number any one of these? " + set2);
+    boolean isWithinSecondSet = scannerObject.next().charAt(0) == 'Y' ? true : false;
+
+    System.out.println("Is the number any one of these? " + set3);
+    boolean isWithinThirdSet = scannerObject.next().charAt(0) == 'Y' ? true : false;
+
+    System.out.println("Is the number any one of these? " + set4);
+    boolean isWithinFourthSet = scannerObject.next().charAt(0) == 'Y' ? true : false;
+
+    System.out.println("Is the number any one of these? " + set5);
+    boolean isWithinFifthSet = scannerObject.next().charAt(0) == 'Y' ? true : false;
+
+    int number = (isWithinFirstSet ? 1 : 0) * Integer.parseInt(set1.substring(0, set1.indexOf(" "))) +
+    (isWithinSecondSet ? 1 : 0) * Integer.parseInt(set2.substring(0, set2.indexOf(" "))) +
+    (isWithinThirdSet ? 1 : 0) * Integer.parseInt(set3.substring(0, set3.indexOf(" "))) +
+    (isWithinFourthSet ? 1 : 0) * Integer.parseInt(set4.substring(0, set4.indexOf(" "))) +
+    (isWithinFifthSet ? 1 : 0) * Integer.parseInt(set5.substring(0, set5.indexOf(" ")));
+
+    System.out.println("The number is " + number);
+  }
+}
---
+++ Liang/Chapter4/HexadecimalDigitToDecimalValue.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class HexadecimalDigitToDecimalValue
+{
+  public static void main(String[] args)
+  {
+    Scanner scannerObject = new Scanner(System.in);
+    System.out.println("Enter a hexadecimal digit. ");
+    String userInput = scannerObject.next();
+
+    if(userInput.length() > 1)
+    {
+      System.out.println("You must enter exactly one hexadecimal character. ");
+    }
+    else if(!(userInput.charAt(0) >= 48 && userInput.charAt(0) <= 57 || userInput.charAt(0) >= 65 && userInput.charAt(0) <= 70 || userInput.charAt(0) >= 97 && userInput.charAt(0) <= 102))
+    {
+      //I know, I know, too many calls to a method. I should store the value of the first character in a variable instead. This is a quick solution, not a comprehensive one.
+      System.out.println("You must enter a valid hexadecimal digit. ");
+    }
+    else
+    {
+      System.out.print("The decimal equivalent: ");
+      switch(userInput.charAt(0))
+      {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          System.out.print(userInput.charAt(0));
+          break;
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+          System.out.println(userInput.charAt(0) - 87);
+          break;
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+          System.out.println(userInput.charAt(0) - 55);
+          break;
+      }
+    }
+  }
+}
