9531cdd7729ab366c700a22aa8d50cc91ad612e5
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-08-01 20:46:17 +0530

    Revising through covered portions of textbook

---
+++ Liang/Chapter4/EscapingPercentageSymbol.java
@@ -0,0 +1,10 @@
+public class EscapingPercentageSymbol
+{
+  public static void main(String[] args)
+  {
+    //System.out.printf("\%");
+    System.out.printf("%%");
+  //  System.out.printf("\&");
+    System.out.printf("&&");
+  }
+}
