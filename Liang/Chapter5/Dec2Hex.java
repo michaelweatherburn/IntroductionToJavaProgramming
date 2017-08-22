ccdb7f160f567ebbad9a1e8dfb9bc5a0c9f96969
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-08-22 21:29:20 +0530

    Working on converters between different radices

---
+++ Liang/Chapter5/Dec2Hex.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Dec2Hex
+{
+	public static final int RADIX = 16;
+	public static final int DECIMAL_RADIX = 10;
+	public static void main(String[] args)
+	{
+		Scanner scannerObject = new Scanner(System.in);
+		
+		String hexadecimalEquivalent = "";
+
+		System.out.println("Enter the decimal number. ");
+		int decimalNumber = scannerObject.nextInt();
+		String remainder = null;
+
+		while(decimalNumber != 0)
+		{
+			if(decimalNumber % RADIX >= 0 && decimalNumber % RADIX <= 9)
+				remainder = Integer.toString(decimalNumber % RADIX);
+			else
+				remainder = Character.toString((char)(decimalNumber % RADIX  + 'A' - DECIMAL_RADIX));
+			
+			hexadecimalEquivalent = remainder + hexadecimalEquivalent;
+			System.out.println(hexadecimalEquivalent);
+			decimalNumber = decimalNumber / RADIX;
+		}
+
+		System.out.println("The hexadecimal equivalent: " + hexadecimalEquivalent);
+
+	}
+}
