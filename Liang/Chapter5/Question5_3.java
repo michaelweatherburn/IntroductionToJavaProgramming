165eccbdc2f1e58eb514f0361818fb50c3c929bd
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-11-09 21:01:54 +0530

    Continuing Chapter 5 exercise

---
+++ Liang/Chapter5/Question5_3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Question5_3
+{
+	public static void main(String[] args)
+	{
+		Scanner scannerObject = new Scanner(System.in);
+		System.out.printf("%-8s%8s\n", "Kilograms", "Pounds");
+		for (int i = 1; i < 200; i++)
+		{
+			System.out.printf("%-8d%8.1f\n", i, (i * 2.2));
+		}
+	}
+}
---
+++ Liang/Chapter5/Question5_4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Question5_4
+{
+	public static void main(String[] args)
+	{
+		Scanner scannerObject = new Scanner(System.in);
+		System.out.printf("%-10s%-10s\n", "Miles", "Kilometers");
+		for (int i = 1; i <= 10; i++)
+		{
+			System.out.printf("%-10d%-10.3f\n", i, (i * 1.609));
+		}
+	}
+}
