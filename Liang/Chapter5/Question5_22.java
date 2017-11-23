fb1528e552846ebc87534823cbb98a69cff681c3
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-11-23 09:59:04 +0530

    Amortization, and its relation with annuity

---
+++ Liang/Chapter5/Question5_22.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Question5_22
+{
+	public static void main(String[] args)
+	{
+		Scanner scannerObject = new Scanner(System.in);
+		System.out.print("Loan Amount: ");
+		double principal = scannerObject.nextDouble();
+		System.out.print("Number of Years: ");
+		int numberOfYears = scannerObject.nextInt();
+		int numberOfMonths = numberOfYears * 12;
+		System.out.print("Annual Interest Rate: ");
+		double annualInterestRate = scannerObject.nextDouble();
+		double monthlyInterestRate = annualInterestRate / 12;
+
+		double monthlyPayment = (principal * monthlyInterestRate / 100) / (1 - (1 / Math.pow(1 + monthlyInterestRate / 100, numberOfMonths)));
+		double totalPayment = monthlyPayment * numberOfMonths;
+		System.out.printf("Monthly Payment: %.2f\n", monthlyPayment);
+		System.out.printf("Total Payment: %.2f\n", totalPayment);
+
+		double balance = principal;
+		double monthlyInterest;
+		double monthlyPrincipal;
+
+		System.out.printf("%-12s%-12s%-12s%-12s\n", "Payment#", "Interest", "Principal", "Balance");
+		for (int i = 1; i <= numberOfMonths; i++)
+		{
+			monthlyInterest = balance * monthlyInterestRate / 100;
+			monthlyPrincipal = monthlyPayment - monthlyInterest;
+			balance -= monthlyPrincipal;
+			System.out.printf("%-12d%-12.2f%-12.2f%-12.2f\n", i, monthlyInterest, monthlyPrincipal, balance);
+		}
+	}
+}
