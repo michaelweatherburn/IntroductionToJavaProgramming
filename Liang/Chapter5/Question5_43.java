68dd0c16711fd22e86ac41f2b664c44c7a2f13a3
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2017-12-12 20:20:53 +0530

    Continuing with exercise

---
+++ Liang/Chapter5/Question5_43.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Question5_43
+{
+	public static void main(String[] args)
+	{
+		Scanner scannerObject = new Scanner(System.in);
+		int count = 0;
+		for (int i = 1; i < 7; i++)
+			for (int j = i + 1; j <= 7; j++)
+			{
+				System.out.println(i + " " + j);
+				count++;
+			}
+			System.out.println("The total number of combinations is " + count);
+	}
+}
