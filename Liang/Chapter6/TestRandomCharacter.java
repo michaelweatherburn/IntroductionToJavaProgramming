2aa2882388ba86bffecff65b595a465d91c20db5
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2018-06-22 20:37:59 +0530

    Committing proper TestRandomCharacter

--- Liang/Chapter6/TestRandomCharacter.java
+++ Liang/Chapter6/TestRandomCharacter.java
@@ -1,15 +1,15 @@
-public class TestRandomCharacter
-{
+public class TestRandomCharacter {
 	public static void main(String[] args)
 	{
 		final int NUMBER_OF_CHARACTERS = 175;
 		final int CHARACTERS_PER_LINE = 25;
-
 		for (int i = 0; i < NUMBER_OF_CHARACTERS; i++)
 		{
-			System.out.print(RandomCharacter.getRandomUpperCaseLetter());
+			char ch = RandomCharacter.getRandomLowerCaseLetter();
 			if ((i + 1) % CHARACTERS_PER_LINE == 0)
-				System.out.println();
+				System.out.println(ch);
+			else
+				System.out.print(ch);
 		}
 	}
 }
