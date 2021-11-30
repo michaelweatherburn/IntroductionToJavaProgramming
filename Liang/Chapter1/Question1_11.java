33a4954cb5f2415968fd08d4479743c74b051f1a
Subham Satyajeet <7932787+SubhamSatyajeet@users.noreply.github.com>
2021-11-30 15:19:25 +0530

    Improved counting accuracy by considering fractions of each birth/death/immigration

--- Liang/Chapter1/Question1_11.java
+++ Liang/Chapter1/Question1_11.java
@@ -1,20 +1,27 @@
-public class Question1_11{
-  public static void main(String[] args)
-  {
-    final long CURRENT_POPULATION = 312032486;
-    long runningPopulationCount = CURRENT_POPULATION;
-    long secondsInAYear = 365 * 24 * 60 * 60;
+public class Exercise1_11 {
+	public static void main(String[] args) {
+		final int BIRTHS_PER_SECOND = 7;
+		final int DEATHS_PER_SECOND = 13;
+		final int IMMIGRATIONS_PER_SECOND = 45;
+		final long SECONDS_PER_YEAR = 60 * 60 * 24 * 365;
+		double currentPopulation = 312_032_486;
 
-    long yearlyBirths = secondsInAYear/7;
-    long yearlyDeaths = secondsInAYear/13;
-    long yearlyImmigrants = secondsInAYear/45;
+		double birthsPerYear = SECONDS_PER_YEAR / BIRTHS_PER_SECOND;
+		double deathsPerYear = SECONDS_PER_YEAR / DEATHS_PER_SECOND;
+		double immigrationsPerYear = SECONDS_PER_YEAR / DEATHS_PER_SECOND;
 
-    System.out.println("Before: " + CURRENT_POPULATION);
-    for(int i = 1; i <= 5; i++)
-    {
-      runningPopulationCount = runningPopulationCount + yearlyBirths - yearlyDeaths + yearlyImmigrants;
-      System.out.println("After Year " + i +": " + runningPopulationCount);
-    }
+		double cumulativeBirthCount = 0;
+		double cumulativeDeathCount = 0;
+		double cumulativeImmigrationCount = 0;
 
-  }
+		long runningPopulationCount = (long) currentPopulation;
+		System.out.println("Year 0: " + runningPopulationCount);
+		for (int i = 0; i < 5; i++) {
+			cumulativeBirthCount += birthsPerYear;
+			cumulativeDeathCount += deathsPerYear;
+			cumulativeImmigrationCount += immigrationsPerYear;
+			runningPopulationCount += (long) (Math.floor(cumulativeBirthCount) - Math.floor(cumulativeDeathCount) + Math.floor(cumulativeImmigrationCount));
+			System.out.println("Year " + (i + 1) + ": " + runningPopulationCount);
+		}
+	}
 }
